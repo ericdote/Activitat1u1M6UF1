6ba23e8f5bd939c328cd70ec58de2d7642df7e95
ericdote <rovx15@gmail.com>
2017-02-10 20:45:36 +0100

    Fin

--- src/exercici1/Exercici1.java
+++ src/exercici1/Exercici1.java
@@ -1,15 +1,19 @@
 package exercici1;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.RandomAccessFile;
 
 public class Exercici1 {
 
-
     public static void main(String[] args) throws FileNotFoundException, IOException {
-
+        NombresPrimers np = new NombresPrimers();
+        for (int i = 0; i < 10; i++) {
+            np.generarNombre();
+        }        
+        System.out.println(np.totalNombres());
+        System.out.println(np.nombrePosicio(4));
+        System.out.println(np.utlimNombre());
+        System.out.println(np.cercaNombres(2, 20));
     }
 
 }
--- src/exercici1/NombresPrimers.java
+++ src/exercici1/NombresPrimers.java
@@ -8,95 +8,107 @@ import java.util.ArrayList;
 
 public class NombresPrimers {
 
-    File fitxer = new File("C:\\Users\\Eric\\Desktop\\DAM\\M6\\UF1\\Ficheros\\Exercici1");
-    RandomAccessFile raf;
-    /**
-     * Constructor de la clase que inicializa el raf
-     * @throws FileNotFoundException 
-     */
-    public NombresPrimers() throws FileNotFoundException {
-        this.raf = new RandomAccessFile(fitxer, "rw");
-    }
+    File fitxer = new File("C:\\Users\\Eric\\Desktop\\DAM\\M6\\UF1\\Ficheros\\Exercici1.txt");
+
     /**
      * Metode que afegeix els numeros primers
-     * @throws IOException 
+     *
+     * @throws IOException
      */
     public void generarNombre() throws IOException {
+        RandomAccessFile raf = new RandomAccessFile(fitxer, "rw");
         if (fitxer.length() != 0) {
             raf.seek(fitxer.length() - 4);
             int num = raf.readInt();
             int nextNum = calculNumeroPrimer(num);
             raf.seek(fitxer.length());
             raf.writeInt(nextNum);
+        } else {
+            raf.seek(0);
+            raf.writeInt(2);
         }
     }
+
     /**
      * Metode que calcula els numeros primers
+     *
      * @param lastNum
-     * @return 
+     * @return
      */
-    public int calculNumeroPrimer(int lastNum) {
+    public int calculNumeroPrimer(int lastNum) throws FileNotFoundException {
+        RandomAccessFile raf = new RandomAccessFile(fitxer, "rw");
         boolean next = true;
-        int num = 0;
+        int num = 0;        
         do {
-            if ((lastNum + 1) % 2 == 0) {
+            lastNum++;
+            if ((lastNum) % 2 == 0) {
                 num = lastNum + 1;
                 next = false;
-            }
-            lastNum++;
+            }            
         } while (next);
         return num;
     }
+
     /**
      * Metode que torna el total de numeros que tenim al fitxer
-     * @return 
+     *
+     * @return
      */
-    public long totalNumeros() {
+    public long totalNombres() throws FileNotFoundException {
+        RandomAccessFile raf = new RandomAccessFile(fitxer, "rw");
         long nums = fitxer.length() / 4;
         return nums;
     }
+
     /**
      * Metode que retorna una llista amb els numeros que estan entre 2 valors
+     *
      * @param num1
      * @param num2
      * @return
-     * @throws IOException 
+     * @throws IOException
      */
-    public ArrayList cercaNumeros(int num1, int num2) throws IOException {
-        
+    public ArrayList cercaNombres(int num1, int num2) throws IOException {
+        RandomAccessFile raf = new RandomAccessFile(fitxer, "rw");
         ArrayList numeros = new ArrayList<>();
         boolean numTrobat = true;
         raf.seek(0);
-        while(numTrobat){
+        while (numTrobat) {
             int numero = raf.readInt();
-            if(numero >= num1 && numero <= num2){
+            if (numero >= num1 && numero <= num2) {
                 numeros.add(numero);
-            } else if(numero > num2){
+            } else if (numero > num2) {
                 numTrobat = false;
-            }       
+            }
         }
         raf.close();
         return numeros;
     }
+
     /**
      * Metode que retorna l'utlim numero del fitxer
+     *
      * @return
-     * @throws IOException 
+     * @throws IOException
      */
-    public int ultimNumero() throws IOException{
+    public int utlimNombre() throws IOException {
+        RandomAccessFile raf = new RandomAccessFile(fitxer, "rw");
         int numero;
         raf.seek(raf.length() - 4);
         numero = raf.readInt();
         raf.close();
         return numero;
     }
+
     /**
      * Metode que retorna el numero de una posicio del fitxer
+     *
      * @param posicio
      * @return
-     * @throws IOException 
+     * @throws IOException
      */
-    public int numeroPosicio(int posicio) throws IOException{
+    public int nombrePosicio(int posicio) throws IOException {
+        RandomAccessFile raf = new RandomAccessFile(fitxer, "rw");
         int numero;
         raf.seek((posicio * 4) - 4);
         numero = raf.readInt();
